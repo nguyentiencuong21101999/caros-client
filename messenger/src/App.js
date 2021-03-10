483322dac87d4858d1b2eb6dca4cd0d027751514
Your Name <nguyentiencuong21101999>
2021-03-11 02:16:26 +0700

    a

--- messenger/src/App.js
+++ messenger/src/App.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
-
+import axios  from 'axios'
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data:''
+    }
+  }
+  
+  componentDidMount(){
+    axios.get("/test").then(results =>{
+      console.log(results.data);
+      this.setState({
+        data:results.data.a
+      });
+    })
+  }
   render() {
+    const a = () =>{
+      if(this.state.data != ''){
+        return(
+          <div>
+              {this.state.data}
+          </div>
+        )
+      }
+    }
     return (
       <div>
         Nguyễn Tiến Cường
+        {a()}
       </div>
     );
   }
 }
-
 export default App;
