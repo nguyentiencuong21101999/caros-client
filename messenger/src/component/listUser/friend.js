9edd933790a59e9e42e1f0c999d4dee688f9f794
Your Name <nguyentiencuong21101999>
2021-03-19 20:58:34 +0700

    a

--- messenger/src/component/listUser/friend.js
+++ messenger/src/component/listUser/friend.js
@@ -1,34 +1,35 @@
 import React, { Component } from 'react';
 
 class friend extends Component {
-    
+
     render() {
         const listFriend = () => {
-            return this.props.listFriend.map(element => {
-                return (
-                    <li 
-                   // className="active"
-                    >
-                        <div className="d-flex bd-highlight">
-                            <div className="img_cont">
-                                <img alt="" src={element.image} className="rounded-circle user_img" />
-                                <span className="online_icon" />
+            if (this.props.listFriend.length > 0) {
+                return this.props.listFriend.map(element => {
+                    return (
+                        <li
+                        // className="active"
+                        >
+                            <div className="d-flex bd-highlight">
+                                <div className="img_cont">
+                                    <img alt="" src={element.image} className="rounded-circle user_img" />
+                                    <span className="online_icon" />
+                                </div>
+                                <div className="user_info">
+                                    <span>{element.fullname}</span>
+                                    <p> online</p>
+                                </div>
                             </div>
-                            <div className="user_info">
-                                <span>{element.fullname}</span>
-                                <p> online</p>
-                            </div>
-                        </div>
-                    </li>
-                )
-            })
-
+                        </li>
+                    )
+                })
+            }
         }
         return (
             <div className="card-body contacts_body">
                 <ui className="contacts">
                     {listFriend()}
-                   
+
                 </ui>
             </div>
         );
